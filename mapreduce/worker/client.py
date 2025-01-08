363010d19889a7f36d707253c6790986b1ae7287
Bartosz Zbiciak <bz448593@students.mimuw.edu.pl>
2025-01-08 23:17:54 +0000

    Implement a worker which only replies (#4)
    * Implement a worker which only replies
    
    * Read addresses from argv/env
    
    * Use full reply data in logging
    
    * Specify level in config for logging

---
+++ mapreduce/worker/client.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+import grpc
+from ..grpc import mapreduce_pb2
+from ..grpc import mapreduce_pb2_grpc
+
+def client():
+    if len(sys.argv) != 2:
+        print("usage: ", sys.argv[0], "host:port")
+        return
+
+    with grpc.insecure_channel(sys.argv[1]) as channel:
+        stub = mapreduce_pb2_grpc.NodeAPIStub(channel)
+
+        startStepRequest = mapreduce_pb2.StartStepRequest(
+            inputLocation="inputLocationPlaceholder",
+            outputLocation="outputLocationPlaceholder",
+            stepId="stepIdPlaceholder"
+        )
+        startStepReply = stub.StartStep(startStepRequest)
+        print("StartStep(..):\n" + str(startStepReply))
+
+        nodeStatusReply = stub.NodeStatus(mapreduce_pb2.Empty())
+        print("NodeStatus():\n" + str(nodeStatusReply))
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    client()
---
+++ mapreduce/worker/server.py
@@ -0,0 +1,31 @@
+from concurrent import futures
+import logging
+import os
+import grpc
+from ..grpc import mapreduce_pb2
+from ..grpc import mapreduce_pb2_grpc
+from ..grpc.mapreduce_pb2_grpc import NodeAPIServicer
+
+class NodeAPIServicerImpl(NodeAPIServicer):
+    def StartStep(self, request, context):
+        logging.info("StartStep request:\n" + str(request))
+        return mapreduce_pb2.StartStepReply(ok=True)
+    
+    def NodeStatus(self, request, context):
+        logging.info("NodeStatus request: " + str(request))
+        status = mapreduce_pb2.NodeStatusReply.NodeStatusEnum.Ok
+        return mapreduce_pb2.NodeStatusReply(status=status)
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    mapreduce_pb2_grpc.add_NodeAPIServicer_to_server(
+        NodeAPIServicerImpl(), server
+    )
+    port = os.environ.get("HTTP_PORT", "[::]:50051")
+    server.add_insecure_port(port)
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    serve()
