66cf70a55599d2779dc028936d213b34baf949c6
Bartosz Zbiciak <bz448593@students.mimuw.edu.pl>
2025-01-10 14:05:51 +0100

    Add uuid to workers

--- mapreduce/worker/server.py
+++ mapreduce/worker/server.py
@@ -2,19 +2,24 @@ from concurrent import futures
 import logging
 import os
 import grpc
+import uuid
 from ..grpc import mapreduce_pb2
 from ..grpc import mapreduce_pb2_grpc
 from ..grpc.mapreduce_pb2_grpc import NodeAPIServicer
 
 class NodeAPIServicerImpl(NodeAPIServicer):
+    def __init__(self):
+        super().__init__()
+        self.workerUuid = str(uuid.uuid4())
+
     def StartStep(self, request, context):
         logging.info("StartStep request:\n" + str(request))
-        return mapreduce_pb2.StartStepReply(ok=True)
+        return mapreduce_pb2.StartStepReply(ok=True, workerUuid=self.workerUuid)
     
     def NodeStatus(self, request, context):
         logging.info("NodeStatus request: " + str(request))
         status = mapreduce_pb2.NodeStatusReply.NodeStatusEnum.Ok
-        return mapreduce_pb2.NodeStatusReply(status=status)
+        return mapreduce_pb2.NodeStatusReply(status=status, workerUuid=self.workerUuid)
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -27,5 +32,5 @@ def serve():
     server.wait_for_termination()
 
 if __name__ == "__main__":
-    logging.basicConfig()
+    logging.basicConfig(level=logging.INFO)
     serve()
