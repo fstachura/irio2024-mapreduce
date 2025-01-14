8bf5172bc15f9896729f5336c1687d1947499656
Franciszek Stachura <fbstachura@gmail.com>
2025-01-14 12:33:23 +0100

    proto, worker, coordinator: Split proto files, update code

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
-mapreduce/proto/mapreduce_pb2.py: mapreduce/proto/mapreduce.proto
-	python3 -m grpc_tools.protoc -Imapreduce/proto=mapreduce/proto --python_out=. --grpc_python_out=. mapreduce/proto/mapreduce.proto
+all: mapreduce/proto/coordinator_pb2.py mapreduce/proto/worker_pb2.py
+
+mapreduce/proto/coordinator_pb2.py: mapreduce/proto/coordinator.proto
+	python3 -m grpc_tools.protoc -Imapreduce/proto=mapreduce/proto --python_out=. --grpc_python_out=. $^
+
+mapreduce/proto/worker_pb2.py: mapreduce/proto/worker.proto
+	python3 -m grpc_tools.protoc -Imapreduce/proto=mapreduce/proto --python_out=. --grpc_python_out=. $^
 
 clean:
-	rm mapreduce/proto/*.py
+	rm -f mapreduce/proto/*.py
 
--- mapreduce/worker/server.py
+++ mapreduce/worker/server.py
@@ -3,28 +3,30 @@ import logging
 import os
 import grpc
 import uuid
-from ..proto import mapreduce_pb2
-from ..proto import mapreduce_pb2_grpc
-from ..proto.mapreduce_pb2_grpc import NodeAPIServicer
+from google.protobuf.empty_pb2 import Empty
 
-class NodeAPIServicerImpl(NodeAPIServicer):
+from ..proto import worker_pb2
+from ..proto import worker_pb2_grpc
+from ..proto.worker_pb2_grpc import WorkerServiceServicer
+
+class WorkerServiceServicerImpl(WorkerServiceServicer):
     def __init__(self):
         super().__init__()
         self.workerUuid = str(uuid.uuid4())
 
     def StartStep(self, request, context):
         logging.info("StartStep request:\n" + str(request))
-        return mapreduce_pb2.StartStepReply(ok=True, workerUuid=self.workerUuid)
+        return worker_pb2.StartStepReply(ok=True, workerUuid=self.workerUuid)
 
-    def NodeStatus(self, request, context):
-        logging.info("NodeStatus request: " + str(request))
-        status = mapreduce_pb2.NodeStatusReply.NodeStatusEnum.Ok
-        return mapreduce_pb2.NodeStatusReply(status=status, workerUuid=self.workerUuid)
+    def WorkerStatus(self, request, context):
+        logging.info("WorkerStatus request: " + str(request))
+        status = worker_pb2.WorkerStatusReply.WorkerStatusEnum.Ok
+        return worker_pb2.WorkerStatusReply(status=status, workerUuid=self.workerUuid)
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    mapreduce_pb2_grpc.add_NodeAPIServicer_to_server(
-        NodeAPIServicerImpl(), server
+    worker_pb2_grpc.add_WorkerServiceServicer_to_server(
+        WorkerServiceServicerImpl(), server
     )
     port = os.environ.get("HTTP_PORT", "[::]:50051")
     server.add_insecure_port(port)
