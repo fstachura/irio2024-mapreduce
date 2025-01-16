8009e77f7b1ddd4a493e3055f7e8f071c87bc9e1
Franciszek Stachura <fbstachura@gmail.com>
2025-01-16 23:23:11 +0100

    coordinator: Add shuffle implementation

--- mapreduce/coordinator/server.py
+++ mapreduce/coordinator/server.py
@@ -29,38 +29,97 @@ def start_map(tr, storage_client, job):
             part = JobPart(input_location=f"{bucket_name}:{f.name}",
                            finished=False,
                            job_id=job.id,
-                           step="map")
+                           step=WORDCOUNT_MAP)
             tr.add(part)
 
     tr.commit()
 
+def split_on_same_elements(l, parts, eq):
+    result = []
+    part_len = (len(l)+1)//parts
+    part_idxes = [(n+1)*part_len for n in range(parts-1)]
+    print(part_idxes, part_len)
+
+    for n in range(parts-1):
+        i = part_idxes[n]
+        while i < len(l) and eq(l[i-1], l[i]):
+            i += 1
+        part_idxes[n] = i
+
+    start = 0
+    for idx in part_idxes:
+        result.append(l[start:idx])
+        start = idx
+
+    result.append(l[part_idxes[-1]:])
+    return result
+
 def execute_shuffle(tr, storage_client, job):
-    # download all data (only from job parts)
-    # sort
-    # split into n nodes
-    # upload
-    # create reduce job_part for each output file
-    # all in a transaction - if coordinator crashes, neither the transition nor job parts get saved
-    # what if coordinator fails during that? execute shuffle will get re-executed from the loop (assuming i don't mess up transactions)
-    pass
+    stmt = select(JobPart).where(JobPart.step == WORDCOUNT_MAP, JobPart.job_id == job.id)
+    parts = tr.execute(stmt).scalars().all()
+    result = []
+
+    # NOTE doing an in memory sort and split (which technically defeats the whole purpose) for now
+    for part in parts:
+        assert part.finished
+
+        bucket_name, directory_name = part.output_location.split(':')
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.get_blob(directory_name)
+        with blob.open('r') as f:
+            result.extend((line.split(',') for line in f.read().split('\n') if len(line.strip()) != 0))
+
+    result.sort(key=lambda x: x[0])
+    reduce_parts = split_on_same_elements(result, len(parts), lambda x, y: x[0] == y[0])
+    reduce_parts = [p for p in reduce_parts if len(p) != 0]
+
+    job_bucket_name, input_directory = job.input_location.split(":")
+    job_bucket = storage_client.bucket(job_bucket_name)
+
+    for reduce_part in reduce_parts:
+        # upload somewhere
+        input_location = generate_tmp_location(input_directory , WORDCOUNT_REDUCE + "_input")
+        with job_bucket.blob(input_location).open('w') as f:
+            for part in reduce_part:
+                f.write(part[0] + "," + part[1] + "\n")
+
+        part = JobPart(input_location=f"{job_bucket_name}:{input_location}",
+                       finished=False,
+                       job_id=job.id,
+                       step=WORDCOUNT_REDUCE)
+        tr.add(part)
+
+    job.current_step = WORDCOUNT_REDUCE
+    tr.commit()
+    # in a transaction - if coordinator crashes, neither the transition nor job parts get saved
+
+    # what if coordinator fails here? 
+    # execute shuffle will get re-executed from the loop (assuming i don't mess up transactions). 
+    # some job parts may have been started and lost
 
 def execute_collect(tr, storage_client, job):
     # concat all reduce output files into a new file
     # what if coordinator fails during that? will get re-executed from the loop
     pass
 
+WORDCOUNT_MAP = "map"
+WORDCOUNT_SHUFFLE = "shuffle"
+WORDCOUNT_REDUCE = "reduce"
+WORDCOUNT_COLLECT = "collect"
+
 WORDCOUNT_STEPS = {
-    "map": {
-        "next": "shuffle",
+    WORDCOUNT_MAP: {
+        "next": WORDCOUNT_SHUFFLE,
         "callback": start_map,
     },
-    "shuffle": {
-        "next": "reduce",
+    WORDCOUNT_SHUFFLE: {
         # starts reduce job parts
         "callback": execute_shuffle,
-        "next": "collect"
     },
-    "collect": {
+    WORDCOUNT_REDUCE: {
+        "next": WORDCOUNT_COLLECT,
+    },
+    WORDCOUNT_COLLECT: {
         "callback": execute_collect,
     }
 }
@@ -73,12 +132,12 @@ class CoordinatorServiceServicerImpl(CoordinatorServiceServicer):
         with Session(self.db) as tr:
             storage_client = storage.Client()
 
-            job = Job(input_location=request.inputLocation, current_step="map")
+            job = Job(input_location=request.inputLocation, current_step=WORDCOUNT_MAP)
             tr.add(job)
             tr.flush()
             tr.refresh(job)
 
-            WORDCOUNT_STEPS["map"]["callback"](tr, storage_client, job)
+            WORDCOUNT_STEPS[WORDCOUNT_MAP]["callback"](tr, storage_client, job)
             return StartJobReply(jobUuid=str(job.job_uuid))
 
     def LastJobStatus(self, request, context):
@@ -131,15 +190,15 @@ def restart_part(tr, part):
     part.output_location = None
     tr.commit()
 
-def generate_output_location(input_location, step):
-    return str(Path(input_location).parent).rstrip("/") + f"_output/{step}/" + str(uuid.uuid4())
+def generate_tmp_location(base, step):
+    return base.rstrip("/") + f"_tmp/{step}/" + str(uuid.uuid4())
 
-def submit_job_to_worker(tr, addr, node_port, part):
+def submit_job_to_worker(tr, addr, node_port, job, part):
     try:
         logging.info(f"attempting to submit job to worker {addr} {part.step} {part.input_location}")
         with grpc.insecure_channel(addr + ":" + node_port) as channel:
             stub = WorkerServiceStub(channel)
-            output_location = generate_output_location(part.input_location, part.step)
+            output_location = generate_tmp_location(job.input_location, part.step)
             request = StartStepRequest(
                     stepId=part.step, 
                     inputLocation=part.input_location,
@@ -160,7 +219,7 @@ def submit_job_to_worker(tr, addr, node_port, part):
     except Exception:
         logging.exception(f"failed to submit job to worker {addr}")
 
-def update(tr, get_nodes):
+def update(tr, storage_client, get_nodes):
     # if all parts finished, execute next step - collect all parts, pass to reducer etc
     # NOTE: it is assumed, that while multiple threads can create job parts, only a single thread (at the same time) manipulates job statues.
     nodes = get_node_stats(get_nodes())
@@ -205,7 +264,7 @@ def update(tr, get_nodes):
                 # job was never assigned
                 if len(free_nodes) > 0:
                     node = free_nodes.pop()
-                    submit_job_to_worker(tr, node[0][0], node[0][1], part)
+                    submit_job_to_worker(tr, node[0][0], node[0][1], job, part)
             else:
                 # node went missing
                 # TODO maybe assume that the node went missing after failing to contact it three times in a row?
@@ -213,20 +272,20 @@ def update(tr, get_nodes):
                 tr.commit()
                 if len(free_nodes) > 0:
                     node = free_nodes.pop()
-                    submit_job_to_worker(tr, node[0][0], node[0][1], part)
+                    submit_job_to_worker(tr, node[0][0], node[0][1], job, part)
 
     tr.commit()
 
     if len(get_unfinished_job_parts(tr)) == 0:
         logging.info(f"step finished {job.current_step}")
         next_step_name = WORDCOUNT_STEPS[job.current_step]["next"]
-        WORDCOUNT_STEPS[next_step_name]["callable"]()
+        WORDCOUNT_STEPS[next_step_name]["callback"](tr, storage_client, job)
 
-def start_update_loop(db, get_nodes):
+def start_update_loop(db, storage_client, get_nodes):
     while True:
         try:
             with Session(db) as tr:
-                update(tr, get_nodes)
+                update(tr, storage_client, get_nodes)
         except:
             logging.exception("failed to update")
         sleep(1)
@@ -238,9 +297,10 @@ def serve():
 
         port = os.environ.get("HTTP_PORT", "[::]:50001")
         db = connect_to_db()
+        storage_client = storage.Client()
         #get_nodes = lambda: get_nodes_from_dns(nodes_addr, node_port)
         get_nodes = lambda: [("localhost", "50051"), ("localhost", "50052"), ("localhost", "50053")]
-        executor.submit(start_update_loop, db, get_nodes)
+        executor.submit(start_update_loop, db, storage_client, get_nodes)
 
         server = grpc.server(executor)
         add_CoordinatorServiceServicer_to_server(
