2aaa804dd2c10f3309deb7dc450ea22836c4fe91
Bartosz Zbiciak <bz448593@students.mimuw.edu.pl>
2025-01-16 19:38:28 +0100

    Handle job failures

--- mapreduce/worker/server.py
+++ mapreduce/worker/server.py
@@ -1,4 +1,5 @@
 from concurrent import futures
+from enum import Enum, auto
 import threading
 import logging
 import os
@@ -23,19 +24,52 @@ def get_file_handles_from_gstorage(locationsList):
 
     return file_handles
 
-def map_function(input_file, output_file):
-    with input_file.open('r') as r, output_file.open('w') as w:
-        for line in r:
-            for word in line.split():
-                w.write(f"{word},1\n")
 
-def reduce_function(input_file, output_file):
-    with input_file.open('r') as r, output_file.open('w') as w:
-        line_cnt = 0
-        for line in r:
-            word, cnt = line.split(',')
-            line_cnt += int(cnt)
-        w.write(f"{word},{line_cnt}")
+class Job:
+    class JobStatus(Enum):
+        OK = auto()
+        WORKING = auto()
+        FAILED = auto()
+
+    def __init__(self, target, args):
+        self.thread = threading.Thread(target=target, args=(self, *args))
+        self.exception_string = None
+
+    def status(self):
+        if self.is_working():
+            return self.JobStatus.WORKING
+        elif self.exception_string is None:
+            return self.JobStatus.OK
+        else:
+            return self.JobStatus.FAILED
+
+    def start(self):
+        self.thread.start()
+
+    def is_working(self):
+        return self.thread.is_alive()
+
+    def map_function(self, input_file, output_file):
+        try:
+            with input_file.open('r') as r, output_file.open('w') as w:
+                for line in r:
+                    for word in line.split():
+                        w.write(f"{word},1\n")
+        except Exception as e:
+            self.exception_string = repr(e)
+            raise
+
+    def reduce_function(self, input_file, output_file):
+        try:
+            with input_file.open('r') as r, output_file.open('w') as w:
+                line_cnt = 0
+                for line in r:
+                    word, cnt = line.split(',')
+                    line_cnt += int(cnt)
+                w.write(f"{word},{line_cnt}")
+        except Exception as e:
+            self.exception_string = repr(e)
+            raise
 
 class NodeAPIServicerImpl(NodeAPIServicer):
     def __init__(self):
@@ -43,12 +77,17 @@ class NodeAPIServicerImpl(NodeAPIServicer):
         self.workerUuid = str(uuid.uuid4())
         self.current_job = None
     
+    def status(self):
+        if self.current_job is None:
+            return Job.JobStatus.OK
+        else:
+            return self.current_job.status()
+
     def is_working(self):
         if self.current_job is not None:
-            if self.current_job.is_alive():
+            if self.current_job.is_working():
                 return True
             else:
-                self.current_job = None
                 return False
         else:
             return False
@@ -66,11 +105,11 @@ class NodeAPIServicerImpl(NodeAPIServicer):
 
         match request.stepId:
             case "map":
-                self.current_job = threading.Thread(target=map_function, args=(input_file, output_file))
+                self.current_job = Job(Job.map_function, (input_file, output_file))
                 self.current_job.start()
                 return mapreduce_pb2.StartStepReply(ok=True, workerUuid=self.workerUuid)
             case "reduce":
-                self.current_job = threading.Thread(target=reduce_function, args=(input_file, output_file))
+                self.current_job = Job(Job.reduce_function, (input_file, output_file))
                 self.current_job.start()
                 return mapreduce_pb2.StartStepReply(ok=True, workerUuid=self.workerUuid)
             case stepId:
@@ -81,12 +120,25 @@ class NodeAPIServicerImpl(NodeAPIServicer):
         logging.info("NodeStatus request: " + str(request))
         
         status = mapreduce_pb2.NodeStatusReply.NodeStatusEnum.Unspecified
-        if self.is_working():
-            status = mapreduce_pb2.NodeStatusReply.NodeStatusEnum.Working
-        else:
-            status = mapreduce_pb2.NodeStatusReply.NodeStatusEnum.Ok
+        error_msg = None
+        match self.status():
+            case Job.JobStatus.OK:
+                status = mapreduce_pb2.NodeStatusReply.NodeStatusEnum.Ok
+                self.current_job = None
+            case Job.JobStatus.WORKING:
+                status = mapreduce_pb2.NodeStatusReply.NodeStatusEnum.Working
+            case Job.JobStatus.FAILED:
+                status = mapreduce_pb2.NodeStatusReply.NodeStatusEnum.Failure
+                error_msg = self.current_job.exception_string
+                self.current_job = None
+            case _:
+                raise NotImplementedError("Unhandled worker status")
 
-        return mapreduce_pb2.NodeStatusReply(status=status, workerUuid=self.workerUuid)
+        return mapreduce_pb2.NodeStatusReply(
+            status=status,
+            errorMessage=error_msg,
+            workerUuid=self.workerUuid
+        )
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
