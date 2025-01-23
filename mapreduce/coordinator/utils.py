c6145094deab332540554d6439ee02efe5326585
Franciszek Stachura <fbstachura@gmail.com>
2025-01-23 22:48:51 +0100

    coordinator: Fix split_on_same_elements single bucket edge case

--- mapreduce/coordinator/utils.py
+++ mapreduce/coordinator/utils.py
@@ -1,26 +1,30 @@
 import uuid
+from math import ceil
 from google.cloud import storage
 from sqlalchemy import select
 
 from .database import Job, JobPart
 
 def split_on_same_elements(l, parts, eq):
+    if parts == 1:
+        return l
+
     result = []
-    part_len = (len(l)+1)//parts
-    part_idxes = [(n+1)*part_len for n in range(parts-1)]
+    part_len = ceil(len(l)/parts)
+    part_end_idxes = [(n+1)*part_len for n in range(parts-1)]
 
     for n in range(parts-1):
-        i = part_idxes[n]
+        i = part_end_idxes[n]
         while i < len(l) and eq(l[i-1], l[i]):
             i += 1
-        part_idxes[n] = i
+        part_end_idxes[n] = i
 
     start = 0
-    for idx in part_idxes:
+    for idx in part_end_idxes:
         result.append(l[start:idx])
         start = idx
 
-    result.append(l[part_idxes[-1]:])
+    result.append(l[part_end_idxes[-1]:])
     return result
 
 def generate_tmp_location(base, step):
