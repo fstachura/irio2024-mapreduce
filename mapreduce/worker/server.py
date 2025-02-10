bfb1bba5fc542123d81b6af7da791eabaf0b91e3
Franciszek Stachura <fbstachura@gmail.com>
2025-02-10 10:59:01 +0100

    worker: Add gRPC heatlhcheck service

--- mapreduce/worker/server.py
+++ mapreduce/worker/server.py
@@ -2,6 +2,9 @@ from concurrent import futures
 import logging
 import os
 import grpc
+from grpc_health.v1.health import HealthServicer
+from grpc_health.v1.health_pb2 import HealthCheckResponse
+from grpc_health.v1.health_pb2_grpc import add_HealthServicer_to_server
 
 from .worker_service import WorkerServiceServicerImpl
 from ..proto import worker_pb2_grpc
@@ -12,6 +15,13 @@ def serve():
     worker_pb2_grpc.add_WorkerServiceServicer_to_server(
         WorkerServiceServicerImpl(), server
     )
+
+    health_servicer = HealthServicer()
+    health_servicer.set("WorkerService", HealthCheckResponse.SERVING)
+    add_HealthServicer_to_server(
+        health_servicer, server
+    )
+
     port = os.environ.get("HTTP_PORT", "[::]:50051")
     server.add_insecure_port(port)
     server.start()
